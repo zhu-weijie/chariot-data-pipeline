fca26e9cfc161b49e11f5e73b0e30a3249a33fa6
zhuwj <byzhubaiyuan@gmail.com>
2025-07-05 09:03:24 +0800

    Create the mysql_init.sql Script

---
+++ scripts/mysql_init.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS chariot_db;
+USE chariot_db;
+
+CREATE TABLE movies (
+    movieId INT PRIMARY KEY,
+    title VARCHAR(255),
+    genres VARCHAR(255)
+);
+
+CREATE TABLE ratings (
+    userId INT,
+    movieId INT,
+    rating DECIMAL(2,1),
+    timestamp BIGINT,
+    PRIMARY KEY (userId, movieId)
+);
+
+LOAD DATA INFILE '/var/lib/mysql-files/movies.csv'
+INTO TABLE movies
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/ratings.csv'
+INTO TABLE ratings
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
