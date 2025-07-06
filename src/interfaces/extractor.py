1ab111931e9ff9b0c605e3842f2aeba8c7a249fc
zhuwj <byzhubaiyuan@gmail.com>
2025-07-06 14:15:52 +0800

    Add get_next_high_water_mark interface

--- src/interfaces/extractor.py
+++ src/interfaces/extractor.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
-from typing import List, Dict
+from typing import List, Dict, Any
 
 
 class Extractor(ABC):
     @abstractmethod
-    def read_batch(self, batch_size: int, high_water_mark: int) -> List[Dict]:
+    def read_batch(self, batch_size: int, high_water_mark: Any) -> List[Dict]:
+        pass
+
+    @abstractmethod
+    def get_next_high_water_mark(self, batch: List[Dict]) -> Any:
         pass
